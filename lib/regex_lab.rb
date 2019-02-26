baa0e51717840f8c7f95e813426085c9f4b19d88
Paige Wiener <snowplushie@yahoo.com>
2019-02-26 20:28:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,6 +26,6 @@ end
 
 def valid_phone_number?(phone)
   
-  phone.match(/^[0-9] *?){10}|(\[0-9]{3}\)((\[0-9]{3}\)\[0-9]{4}\|(\[0-9]{7})\b)/) ? true : false
+  phone.match(/^[0-9] *?){10}|(\[0-9]{3}\)((\[0-9]{3}\)\[0-9]{4}\)|(\[0-9]{7})\b)/) ? true : false
 
 end
